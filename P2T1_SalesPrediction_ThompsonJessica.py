6d11b5a90b1a47e3ca0b28a3ccdf1e527a2c5067
thompsoj5563 <36576428+thompsoj5563@users.noreply.github.com>
2018-02-17 16:58:39 -0500

    P2T1 First Submission

---
+++ P2T1_SalesPrediction_ThompsonJessica.py
@@ -0,0 +1,12 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Jessica Thompson
+# 2/13/2018
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
