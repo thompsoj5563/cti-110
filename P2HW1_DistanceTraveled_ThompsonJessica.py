b0f58af6c2b1e8b69b907e066247e6cc5e66a418
thompsoj5563 <36576428+thompsoj5563@users.noreply.github.com>
2018-04-06 18:39:52 -0400

    Fourth submission

---
+++ P2HW1_DistanceTraveled_ThompsonJessica.py
@@ -0,0 +1,22 @@
+# CTI-110 
+# P2HW1 - Distance Traveled  
+# Jessica Thompson
+# 2/13/2018
+#
+#A car is traveling at 70 miles per hour. Display the following:
+#The distance the car will travel in 6 hours
+#The distance the car will travel in 10 hours
+#The distance the car will travel in 15 hours
+
+print('What is the distance a car will go after 6 hours, 10 hours, and 15 hours, if the car is traveling at 70 MPH?')
+speed = 70
+time1 = 6
+time2 = 10
+time3 = 15
+distanceafter6 = speed * time1
+distanceafter10 = speed * time2
+distanceafter15 = speed * time3
+
+print('the car will travel', distanceafter6, 'miles in 6 hours.')
+print('the car will travel', distanceafter10, 'miles in 10 hours.')
+print('the car will travel', distanceafter15, 'miles in 15 hours.')
