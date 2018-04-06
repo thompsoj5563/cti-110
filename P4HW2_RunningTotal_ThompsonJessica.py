16df7482057058fd910b83b612cc5eb982d377e8
thompsoj5563 <36576428+thompsoj5563@users.noreply.github.com>
2018-04-06 18:43:22 -0400

    Eighth Submission

---
+++ P4HW2_RunningTotal_ThompsonJessica.py
@@ -0,0 +1,14 @@
+#CTI 110
+#Jessica Thompson
+#March 17, 2018
+#P4HW2 - Running Total
+
+a = 1
+total = 0
+
+while a > 0:
+    print(" ")
+    a = input("Enter a number?: ")
+    a = float(a)
+    total = a + total
+print("Total: ", total)
